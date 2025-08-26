ed8ceace68cd79d9f3d8d67f253b7a5c2b9e5148
KishorKumarIhub <86336555+KishorKumar07@users.noreply.github.com>
2025-08-26 15:28:25 +0530

    MBA-5  completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
 import About from "./components/About";
 import Skills from "./components/Skills";
+
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -19,6 +20,7 @@ import { Toaster } from 'react-hot-toast';
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
+
   overflow-x: hidden;
 `
 
