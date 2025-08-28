fa1e49f403ca00d34646c0392d07426e74b08f13
KishorKumarIhub <kishor.r.ihub@snsgroups.com>
2025-08-28 13:30:49 +0530

    mba-3 completed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import HeroSection from "./components/HeroSection";
 import About from "./components/About";
-import  Skills from    "./components/Skills";
+import  Skills  from    "./components/Skills";
 
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
