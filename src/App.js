005f0d4bb101e4f888a4c23aef3877bd4580b1e2
KishorKumarIhub <kishorr21cse@gmail.com>
2025-08-28 13:35:28 +0530

    mba-5 completed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import  Skills  from    "./components/Skills";
 
 import Projects from  "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import Footer  from "./components/Footer";
 import Experience  from "./components/Experience";
 import Education from "./components/Education";
 import ProjectDetails    from "./components/ProjectDetails";
